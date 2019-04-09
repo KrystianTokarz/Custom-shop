134818336b80b25c9cadf6d895f3a75c5ecf64a1
Krystian <krystiantokarz@wp.pl>
2019-04-09 19:43:15 +0200

    Backend application initialize commit

---
+++ online-shop-backend/src/main/java/pl/krystiantokarz/onlineshop/configuration/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package pl.krystiantokarz.onlineshop.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    public void test() {
+
+    }
+}
